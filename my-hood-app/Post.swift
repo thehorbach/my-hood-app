62e42b730a4e7e3ec51ffc9a1e01a97e384c870b
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-14 21:24:36 +0000

    added UI of the first story board

---
+++ my-hood-app/Post.swift
@@ -0,0 +1,22 @@
+//
+//  Post.swift
+//  my-hood-app
+//
+//  Created by Vyacheslav Horbach on 14/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    
+    private var imagePath: String
+    private var title: String
+    private var postDesc: String
+    
+    init(imagePath: String, title: String, postDesc: String) {
+        self.imagePath = imagePath
+        self.title = title
+        self.postDesc = postDesc
+    }
+}
