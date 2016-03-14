945ecac0832a259f93bc7a152012dd10328981ef
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-14 22:57:32 +0000

    added MVC + addedcells to tableview and tested them

---
+++ my-hood-app/PostCell.swift
@@ -0,0 +1,28 @@
+//
+//  PostCell.swift
+//  my-hood-app
+//
+//  Created by Vyacheslav Horbach on 14/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    func configureCell(post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+    }
+
+}
--- my-hood-app/ViewController.swift
+++ my-hood-app/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        let post = Post(imagePath: "", title: "Post 1", postDesc: "Some rdn description")
+        let post2 = Post(imagePath: "", title: "Post 2", postDesc: "Another rnd description")
+        let post3 = Post(imagePath: "", title: "Post 3", postDesc: "This is the least important description")
+        
+        posts.append(post)
+        posts.append(post2)
+        posts.append(post3)
+        
+        tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        if let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as? PostCell {
+            cell.configureCell(post)
+            return cell
+        } else {
+            let cell = PostCell()
+            cell.configureCell(post)
+            return cell
+        }
+    }
+    
 
-
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 87.0
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
 }
 
