cc6ca0ff4c62b611bdb838f3d2558c649eb1bcce
Vyacheslav Horbach <vjacheslav.horbach@gmail.com>
2016-03-15 23:31:39 +0000

    added second VC to add new posts

---
+++ my-hood-app/AddPostVC.swift
@@ -0,0 +1,39 @@
+//
+//  AddPostVC.swift
+//  my-hood-app
+//
+//  Created by Vyacheslav Horbach on 15/03/16.
+//  Copyright © 2016 Vyacheslav Horbach. All rights reserved.
+//
+
+import UIKit
+
+class AddPostVC: UIViewController {
+
+    @IBOutlet weak var postImg: UIImageView!
+    
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var descriptionField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func makePostBtnPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func cancelBtnPressed(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    @IBAction func addPicBtnPressed(sender: UIButton!) {
+        sender.setTitle("", forState: .Normal)
+    }
+    
+}
